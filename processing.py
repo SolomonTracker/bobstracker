a36fd8860e4ab494e0be3ceb73a8b7acf57a6753
D6player <victor.mendez@inscanroca.com>
2024-11-30 15:35:29 +0100

    Functional signature checker

--- processing.py
+++ processing.py
@@ -15,11 +15,14 @@ def test():
     pic_arr = np.array(pic, dtype=np.uint8).reshape(-1)
 
     # generate signature
-    signature = gen_signatures(rng, w*h, w, 1, ["WhoCares"])[0]
+    names = ["WhoCares", "SomeoneDoes"]
+    signature = gen_signatures(rng, w*h, w, len(names))
     
     # sign image
-    pic_arr_signed = sign_image(pic_arr, signature[0]).reshape(h, w)
-    pic_signed = Image.fromarray(pic_arr_signed, )
+    # pic_arr_signed = sign_image(pic_arr, signature[0])
+    pic_arr_signed = sign_image(pic_arr, signature[0])
+    assert (check_signature(pic_arr, pic_arr_signed, signature[0]))
+    pic_signed = Image.fromarray(pic_arr_signed.reshape(h, w))
 
     # save it
     pic_signed.save("out_test.png")
@@ -32,30 +35,32 @@ def sign_image(image, signature):
     signature: hashmap( (j: int) -> bool )
     """
     
-    return np.array([x if signature.get(j, 0) == 0 else 0 for j, x in enumerate(image)], dtype=np.uint8)
+    return np.array([x if signature.get(j, 0) == 0 else (int(x)+10)%255 for j, x in enumerate(image)], dtype=np.uint8)
 
-def check_signature(image, signature) -> bool:
+def check_signature(orig_image, image, signature) -> bool:
     """
     image: in array form [uint8]
     signature: hashmap( (j: int) -> bool )
     """
+    
+    arr1 = [x for L, x in enumerate(sign_image(orig_image, signature)) if L in signature]
+    arr2 = [x for L, x in enumerate(image) if L in signature]
+    
+    return arr1 == arr2
 
-    return True
-
-def gen_signatures(rng, max_range, sign_len, ammount, names):
+def gen_signatures(rng, max_range, sign_len, ammount):
     """
     rng: np.random.default_rng()
     max_range: maximum index
     sign_len: ammount of index per signature
     ammount: number of signatures
-    names: Ex ["AWS", "Vitol", ···]
     """
 
     numbers = np.arange(0, max_range)
 
     list_of_indexes = [sorted(rng.choice(numbers, size=sign_len, replace=False)) for _ in range(ammount)]
 
-    return [ ({j:randint(0, 1) for j in i}, names[L]) for L, i in enumerate(list_of_indexes)]
+    return [ {j:randint(0, 1) for j in i} for i in list_of_indexes ]
 
 
 if __name__ == "__main__":
